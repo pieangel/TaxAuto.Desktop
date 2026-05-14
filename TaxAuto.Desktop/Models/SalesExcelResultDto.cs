20cd864d57c360fd7b2b1b3bcf4476425ba17822
angelpie <pieangel@naver.com>
2026-05-14 09:33:36 +0900

    매출 클래스 추가.

---
+++ TaxAuto.Desktop/Models/SalesExcelResultDto.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TaxAuto.Desktop.Models
+{
+    public class SalesExcelResultDto
+    {
+        [JsonPropertyName("input_file")]
+        public string? InputFile { get; set; }
+
+        [JsonPropertyName("date")]
+        public string? Date { get; set; }
+
+        [JsonPropertyName("customer_name")]
+        public string? CustomerName { get; set; }
+
+        [JsonPropertyName("manager")]
+        public string? Manager { get; set; }
+
+        [JsonPropertyName("storage_no")]
+        public string? StorageNo { get; set; }
+
+        [JsonPropertyName("job_no")]
+        public string? JobNo { get; set; }
+
+        [JsonPropertyName("total_amount")]
+        public int? TotalAmount { get; set; }
+
+        [JsonPropertyName("calculated_total")]
+        public int? CalculatedTotal { get; set; }
+
+        [JsonPropertyName("discount_amount")]
+        public int? DiscountAmount { get; set; }
+
+        [JsonPropertyName("total_check")]
+        public bool? TotalCheck { get; set; }
+
+        [JsonPropertyName("needs_review")]
+        public bool NeedsReview { get; set; }
+
+        [JsonPropertyName("review_reason")]
+        public List<string> ReviewReason { get; set; } = new();
+
+        [JsonPropertyName("items")]
+        public List<SalesItemDto> Items { get; set; } = new();
+    }
+}
---
+++ TaxAuto.Desktop/Models/SalesItemDto.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TaxAuto.Desktop.Models
+{
+    public class SalesItemDto
+    {
+        [JsonPropertyName("item_name_raw")]
+        public string? ItemNameRaw { get; set; }
+
+        [JsonPropertyName("spec_raw")]
+        public string? SpecRaw { get; set; }
+
+        [JsonPropertyName("quantity_raw")]
+        public string? QuantityRaw { get; set; }
+
+        [JsonPropertyName("unit_price_raw")]
+        public string? UnitPriceRaw { get; set; }
+
+        [JsonPropertyName("amount_raw")]
+        public string? AmountRaw { get; set; }
+
+        [JsonPropertyName("item_name")]
+        public string? ItemName { get; set; }
+
+        [JsonPropertyName("spec")]
+        public string? Spec { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int? Quantity { get; set; }
+
+        [JsonPropertyName("unit_price")]
+        public int? UnitPrice { get; set; }
+
+        [JsonPropertyName("amount")]
+        public int? Amount { get; set; }
+
+        [JsonPropertyName("calculated_amount")]
+        public int? CalculatedAmount { get; set; }
+
+        [JsonPropertyName("amount_check")]
+        public bool? AmountCheck { get; set; }
+
+        [JsonPropertyName("needs_review")]
+        public bool NeedsReview { get; set; }
+
+        [JsonPropertyName("review_reason")]
+        public List<string> ReviewReason { get; set; } = new();
+    }
+}
